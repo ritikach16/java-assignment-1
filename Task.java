9adef33d220eb168327004791d4eb538cb86c776
Ritika Chauhan <ritika.chauhan@zemosolabs.com>
2022-03-15 19:13:43 +0530

    assignment 1

--- Task.java
+++ Task.java
@@ -1,36 +1,59 @@
 import java.io.File;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Task {
-    static Pattern pathPattern;
-    public static boolean isFilePath(String search, String s){
-        pathPattern = Pattern.compile("([\\w\\s]+(\\.(?i)(" + search + "))$)");
-        Matcher match = pathPattern.matcher(search);
-        if(match.matches()) return true;
-//        if(s.equals(search)) return true;
-        else return false;
-    }
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        while(true) {
-            String search = scn.nextLine();
-            String root = System.getProperty("user.home");
-            boolean flag = false;
-            File file = new File(root);
-            String[] fileList = file.list();
-            for (int i = 0; i < fileList.length; i++) {
-                if (isFilePath(search, fileList[i])) {
-                    flag = true;
+    public void searchFile()
+    {
+
+        File file = new File("C:\\");
+
+
+        String[] files = file.list();
+        System.out.println(Arrays.toString(files));
+
+        Scanner keyBoard = new Scanner(System.in);
+
+        while(true){
+            boolean isFilePresent = false;
+
+
+            String pattern = keyBoard.nextLine();
+
+            System.out.println(Arrays.toString(files));
+
+            for (String string : files){
+                boolean isMatch = string.matches(pattern);
+
+                if (isMatch){
+                    isFilePresent=true;
+                    System.out.println("File Found : ");
+                    System.out.println(string);
+                    System.out.println("Path : "+ String.valueOf(file.getAbsolutePath())+"\\"+string);
                 }
             }
 
-            if (flag) System.out.println(file.getAbsolutePath() + "/" + search);
-            else {
-                System.out.println("File not found!");
-                break;
+
+            if(isFilePresent)
+            {
+                System.out.println("Do you want to continue Search ");
+                System.out.println("Press 1 to continue else press any key to exit");
+                int keyPress=keyBoard.nextInt();
+                keyBoard.nextLine();
+                if(keyPress!=1){
+                    break;
+                }
+
+            }
+            else{
+                System.out.println("File not found!!");
             }
         }
+    }
+
+    public static void main(String[] args){
+        Task findFile =new Task();
+
+        findFile.searchFile();
     }
 }
